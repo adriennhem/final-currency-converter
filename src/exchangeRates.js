97acf1db43f7162f4e547eabfcfde92f5fb43307
adrien nhem <adrien.nhem@gmail.com>
2020-11-05 15:19:37 +0000

    add basic exchange rate table

--- src/exchangeRates.js
+++ src/exchangeRates.js
@@ -1,7 +1,74 @@
-const ExchangeRates = () => (
-    <div>
-        <h2>Exchange Rates</h2>
-    </div>
-)
+import React from 'react';
+import { checkStatus, json } from './utils.js';
+
+class ExchangeRates extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            baseCurrency: 'USD',
+            rates: [],
+            error: ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        
+    }
+
+    componentDidMount() {
+        fetch(`https://alt-exchange-rate.herokuapp.com/latest?base=${this.state.baseCurrency}`)
+            .then(checkStatus)
+            .then(json)
+            .then((data) => {
+                this.setState({ rates: data.rates, error: '' });
+                console.log(data.rates);
+            })
+            .catch(function(err) {
+                this.setState({ error: err.message})
+                console.log(err);
+            });
+    }
+
+    handleChange() {
+        // do nothing for now
+    }
+
+    render() {
+        const { baseCurrency, rates, error } = this.state;
+
+        return (
+            <div>
+            <h2>Exchange Rates</h2>
+            <select name="baseCurrency" id="baseCurrency">
+                {(() => {
+                    if (error) {
+                        return error;
+                    }
+                    const currenciesList = Object.keys(rates).map(function(key) {
+                        if (baseCurrency === key) {
+                            return <option key={key} value={key} selected='selected'>{key}</option>
+                        }
+
+                        return <option key={key} value={key}>{key}</option>
+                    })
+                    return currenciesList;
+                })()}
+            </select>
+            
+                {(() => {
+                if (error) {
+                    return error;
+                }
+                
+                const myRates = Object.keys(rates).map(function(key) {
+                    return <p key={key}>{key }{rates[key]}</p>
+                });
+
+                return myRates;
+                })()}
+            </div>
+        )
+    }
+    
+}
 
 export default ExchangeRates;
