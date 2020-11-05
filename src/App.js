dd8f63dd5ad90de2345b7bf714e3cdc5e97c4f88
adrien nhem <adrien.nhem@gmail.com>
2020-11-05 10:10:03 +0000

    finish the footer

--- src/App.js
+++ src/App.js
@@ -14,17 +14,17 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          <Link to='/' className="navbar-brand">MyCurrencyConverter</Link>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <Link to='/' className="navbar-brand font-weight-bold">HowMuch?</Link>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ml-auto">
-              <li class="nav-item">
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
                 <Link to='/' className="nav-link">Home</Link>
               </li>
-              <li class="nav-item">
+              <li className="nav-item">
                 <Link to='/exchange-rates' className="nav-link">Exchange Rates</Link>
               </li>
             </ul>
--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,26 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedinIn, faTwitter } from '@fortawesome/free-brands-svg-icons'
+
 const Footer = () => (
-    <div className="footer">
-        <p>This is some content</p>
-    </div>
+    <footer className="footer container-fluid">
+        <ul>
+            <li>
+                <a href="https://github.com/adriennhem" target="_blank" rel="noopener noreferrer">
+                  <FontAwesomeIcon icon={faGithub} />
+                 </a>
+            </li>
+            <li>
+                <a href="https://www.linkedin.com/in/adriennhem/" target="_blank" rel="noopener noreferrer">
+                  <FontAwesomeIcon icon={faLinkedinIn} />
+                </a>
+            </li>
+            <li>
+                <a href="https://twitter.com/adriennhem?lang=en" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faTwitter} />
+                </a>
+            </li>
+        </ul>
+    </footer>
 )
 
 export default Footer;
